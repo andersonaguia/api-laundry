3fa5f49292de410e372a5aa21ce2f02092e346d1
Anderson Aguiar <andersonlaguiar@gmail.com>
2023-03-18 12:07:11 -0300

    refactor: modificado retorno da criação do morador

--- src/modules/residents/services/resident.service.ts
+++ src/modules/residents/services/resident.service.ts
@@ -2,6 +2,7 @@ import {
   BadRequestException,
   ConflictException,
   Injectable,
+  NotFoundException,
 } from '@nestjs/common';
 import { ApartmentRepository } from 'src/modules/apartments/apartment.repository';
 import { CreateResidentDto } from '../dto/create-resident.dto';
@@ -22,6 +23,13 @@ export class ResidentService {
       apartment,
     );
 
+    if (!apartmentExists) {
+      throw new NotFoundException({
+        code: 404,
+        message: 'Apartment was not found',
+      });
+    }
+
     const resident = new ResidentEntity();
     const dataResident = {
       ...resident,
@@ -33,6 +41,13 @@ export class ResidentService {
       const residentSaved = await this.residentRepository.createResident(
         dataResident,
       );
+      delete residentSaved.createdAt;
+      delete residentSaved.updatedAt;
+      delete residentSaved.deletedAt;
+      delete residentSaved.apartment.createdAt;
+      delete residentSaved.apartment.updatedAt;
+      delete residentSaved.apartment.deletedAt;
+
       return residentSaved;
     } catch (error) {
       if (error.code === 'ER_DUP_ENTRY') {
